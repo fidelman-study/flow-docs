85ee1362a177498817c2a35102c80d32a1bba911
Andrei Fidelman <andrei.fidelman@actum.cz>
2017-06-12 23:19:38 +0200

    Class Types

--- src/primitive.js
+++ src/primitive.js
@@ -1,20 +1,23 @@
 // @flow
-const tuple1: [number] = [1];
-const tuple2: [number, boolean] = [1, true];
-tuple3.push(3); // Error!
+class MyClass<A, B, C> {
+    constructor(arg1: A, arg2: B, arg3: C) {
+    }
 
-const num: number = tuple2[0];
-const bool: boolean = tuple2[1];
-
-function getItem(n: number) {
-    const val: mixed = tuple2[n];
+    method(value: string): number {
+        return +value;
+    }
 }
 
-tuple1[0] = 1;
+let myInstance: MyClass<number, string, boolean> = new MyClass(1, '1', true);
 
 
-const Tuple1: [number, boolean] = [1, true];
-const Tuple2: [number, boolean, void] = Tuple1; // Error!
 
-const arr: number[] = [1, 2];
-const tupl: [number, number] = array; // Error!
+class Class {
+    props: number = 1;
+
+    constructor(number: number) {
+        this.props = number;
+    }
+}
+
+const hi = new Class(1);
