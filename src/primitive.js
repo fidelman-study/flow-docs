3e4f50a198b915646b57366b8ac65eed2f8d6389
Andrei Fidelman <andrei.fidelman@actum.cz>
2017-06-12 19:00:37 +0200

    Function Types

--- src/primitive.js
+++ src/primitive.js
@@ -1,3 +1,13 @@
 // @flow
-let x: number = 1;
-x = 3;
+const concat = (a: string, b: string): string => a + b;
+concat('1', '2');
+
+function method(str: string, bool?: boolean, ...nums: Array<number>): void {
+    // ...
+}
+method('1', true, 1, 2, 3);
+
+function error(callback: (error: Error | null, value: string | null) => void) {
+    // ...
+}
+error(() => {});
