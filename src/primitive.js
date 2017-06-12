43ceea76439a70014d8d2f227f6f2e4913f1d7b7
Andrei Fidelman <andrei.fidelman@actum.cz>
2017-06-12 23:00:43 +0200

    Tuple Types

--- src/primitive.js
+++ src/primitive.js
@@ -1,15 +1,20 @@
 // @flow
-const Arr1: Array<number> = [1, 2, 3];
-const Arr2: Array<boolean> = [1, 2, 3];
+const tuple1: [number] = [1];
+const tuple2: [number, boolean] = [1, true];
+tuple3.push(3); // Error!
 
-const arr1: number[] = [1, 2, 3];
-const arr2: { foo: string }[] = [{ foo: 'hi' }];
+const num: number = tuple2[0];
+const bool: boolean = tuple2[1];
 
-const arr3: ?number[] = [1, 2];
-
-let array: number[] = [0, 1, 2];
-let value: number | void = array[1];
-
-if (value !== undefined) {
-    // number
+function getItem(n: number) {
+    const val: mixed = tuple2[n];
 }
+
+tuple1[0] = 1;
+
+
+const Tuple1: [number, boolean] = [1, true];
+const Tuple2: [number, boolean, void] = Tuple1; // Error!
+
+const arr: number[] = [1, 2];
+const tupl: [number, number] = array; // Error!
