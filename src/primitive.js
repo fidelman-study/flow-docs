de8aaf5dd4c2704f0ec0262ea270bbf04dbd9f57
Andrei Fidelman <andrei.fidelman@actum.cz>
2017-06-12 18:42:26 +0200

    Variable Types

--- src/primitive.js
+++ src/primitive.js
@@ -1,26 +1,3 @@
 // @flow
-function method<T>(value: T): T {
-  return value; // return the same type as argument
-}
-method(1);
-
-function getTypeOf(value: mixed): string {
-  return typeof value;
-}
-getTypeOf(1);
-
-function stringify(value: mixed) {
-    // $ExpectError
-    return "" + value; // Error!
-}
-stringify("foo");
-
-function stringify1(value: mixed) {
-    if (typeof value === 'string') {
-        return "" + value; // Works!
-    } else {
-        return "";
-    }
-}
-stringify1("foo");
-
+let x: number = 1;
+x = 3;
