25fa94131d6db2784924a90bd3424ff55dbbf8f6
Andrei Fidelman <andrei.fidelman@actum.cz>
2017-06-14 18:04:05 +0200

    Interface Types

--- src/primitive.js
+++ src/primitive.js
@@ -1,24 +1,41 @@
 // @flow
-type MyObject = {
-    foo: number,
-    bar: boolean,
-    baz: string
-};
+interface Serializable {
+    serialize() : string;
+}
 
-const val: MyObject = {
-    foo: 1,
-    bar: true,
-    baz: 'hi'
-};
+class Foo {
+    serialize() { return '[Foo]'; }
+}
 
-type NewObject<A, B, C> = {
-    foo: A,
-    bar: B,
-    baz: C
-};
+class Bar {
+    serialize() { return '[Bar]'; }
+}
 
-const value: NewObject<number, boolean, string> = {
-    foo: 1,
-    bar: true,
-    baz: false // Error!
-};
+const foo: Serializable = new Foo();
+const bar: Serializable = new Bar();
+
+
+
+
+interface Serializable1 {
+    serialize() : string;
+}
+
+class Foo1 implements Serializable {
+    serialize() { return '[Foo]'; }
+}
+
+class Bar1 implements Serializable {
+    serialize() { return '[Bar]'; }
+}
+
+
+interface Invariant { property: number; }
+interface Covariant { +readOnly: number; }
+interface Contravariant { -writeOnly: number; }
+
+var value1: Invariant = { propery: 1 }; // Error!
+var value2: Covariant = { readOnly: 1 };
+var value3: Contravariant = { writeOnly: 1 };
+
+value2.readOnly = 2; // Error!
