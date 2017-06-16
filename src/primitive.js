bcf566150cd02e16b13a05b3f4eb5d780709b725
Andrei Fidelman <andrei.fidelman@actum.cz>
2017-06-16 18:11:35 +0200

    Intersection Types

--- src/primitive.js
+++ src/primitive.js
@@ -1,5 +1,29 @@
 // @flow
-type Numbers = 1 | 2;
-type Colors = 'red' | 'blue'
-type Fish = Numbers | Colors;
+type A = { a: number };
+type B = { b: boolean };
+type C = { c: string };
 
+function method(value: A & B & C) {
+    // ...
+}
+
+method({ a: 1 }); // Error!
+method({ a: 1, b: true }); // Error!
+method({ a: 1, b: true, c: 'three' }); // Works!
+
+
+type Foo = number & string;
+type Bar = boolean & Array<number>;
+
+type Baz = Foo & Bar;
+
+
+type One = { foo: number };
+type Two = { bar: boolean };
+
+type Both = One & Two;
+
+var value: Both = {
+    foo: 1,
+    bar: true
+};
