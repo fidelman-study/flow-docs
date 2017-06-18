95beca91985c54eab8ad1e35d56e5ab0941cf1f9
Andrei Fidelman <andrei.fidelman@actum.cz>
2017-06-18 12:51:19 +0200

    Type Casting Expressions

--- src/primitive.js
+++ src/primitive.js
@@ -1,29 +1,6 @@
 // @flow
-type A = { a: number };
-type B = { b: boolean };
-type C = { c: string };
+let value = 42;
 
-function method(value: A & B & C) {
-    // ...
-}
-
-method({ a: 1 }); // Error!
-method({ a: 1, b: true }); // Error!
-method({ a: 1, b: true, c: 'three' }); // Works!
-
-
-type Foo = number & string;
-type Bar = boolean & Array<number>;
-
-type Baz = Foo & Bar;
-
-
-type One = { foo: number };
-type Two = { bar: boolean };
-
-type Both = One & Two;
-
-var value: Both = {
-    foo: 1,
-    bar: true
-};
+(value: 42);     // Works!
+(value: number); // Works!
+(value: string); // Error!
